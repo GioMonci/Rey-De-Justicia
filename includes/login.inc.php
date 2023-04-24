3d8942d1d674cd536ca957ebc84940d2e8bb0ec3
Giovanni Moncibaez <95672640+GioMonci@users.noreply.github.com>
2023-04-24 05:53:23 -0400

    Update login.inc.php

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -5,8 +5,8 @@ if(isset($_POST['login-submit'])){
 
     require 'dbh.inc.php';
 
-    $mailuid = $_POST['mailuid'];
-    $password = $_POST['pwd'];
+    $mailuid = mysqli_real_escape_string($conn,$_POST['mailuid']);
+    $password = mysqli_real_escape_string($conn,$_POST['pwd']);
 
     if(empty($mailuid) || empty($password)){
         header("Location: ../donations.php?error=emptyFields");
